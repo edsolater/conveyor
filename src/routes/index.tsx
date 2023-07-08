eb52cce28ce36b6f2ec92c4132cd1389ab813af0
edsoalter <edsolater@qq.com>
2023-07-08 20:23:11 +0800

    🎉 init 4

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,34 @@
-import { Title } from 'solid-start'
-import Counter from '~/components/Counter'
+import { createSignal } from 'solid-js'
+import { NavBar } from '~/components/NavBar'
+import { linkCards } from '~/configs/linkCards'
+import { useSearch } from '~/packages/features/search'
+import { Piv } from '~/packages/piv'
+import { Box, Card, Input, List, Section, icss_card, icss_row, Text } from '~/packages/pivkit'
+
 export default function Home() {
+  const [searchText, setSearchText] = createSignal<string>()
+  const links = useSearch(() => linkCards, searchText, { matchConfigs: [(i) => i.name, (i) => i.keywords] })
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{' '}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{' '}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+    <Piv>
+      <NavBar title="Home" />
+      <Section icss={{ display: 'grid', justifyContent: 'center' }}>
+        <Box icss={[icss_row({ gap: '4px' }), { marginBottom: '8px' }]}>
+          <Text>search tags:</Text>
+          <Input onUserInput={({ text }) => setSearchText(text)} />
+        </Box>
+        <Piv icss={{ width: '80vw', height: '60vh' }}>
+          <List items={links}>
+            {(item) => (
+              <Card icss={[icss_card, icss_row({ gap: '.5em', items: 'center' })]}>
+                <Text icss={{ fontSize: '2em', fontWeight: 'bold' }}>{item.name}</Text>
+                <List icss={icss_row({ gap: '.5em' })} items={item.keywords}>
+                  {(keyword) => <Text icss={{ fontSize: '1em' }}>{keyword}</Text>}
+                </List>
+              </Card>
+            )}
+          </List>
+        </Piv>
+      </Section>
+    </Piv>
   )
 }
