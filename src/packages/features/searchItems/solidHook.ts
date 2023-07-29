44a630c5d990132e9c69ed6c50c0955628be11a6
zhanggongying <edsolater@qq.com>
2023-07-29 13:38:45 +0800

    🚧 useSearch use createDeffered

--- src/packages/features/searchItems/solidHook.ts
+++ src/packages/features/searchItems/solidHook.ts
@@ -1,12 +1,12 @@
-import { createMemo } from 'solid-js'
+import { createDeferred, createMemo } from 'solid-js'
 import { SearchOptions, searchItems } from './core'
 import { MayFn, isArray, shrinkFn } from '@edsolater/fnkit'
 
 export function useSearch<T>(items: (() => T[]) | T[], text: MayFn<string | undefined>, options?: SearchOptions<T>) {
-  const allItems = isArray(items) ? items : items()
+  const allItems = createDeferred(() => (isArray(items) ? items : items()))
   const searchedItems = createMemo(
-    () => searchItems(allItems, { ...options, text: shrinkFn(text) }),
-    searchItems(allItems, { ...options, text: shrinkFn(text) }),
+    () => searchItems(allItems(), { ...options, text: shrinkFn(text) }),
+    searchItems(isArray(items) ? items : items(), { ...options, text: shrinkFn(text) }),
     { equals: (prev, next) => prev.length === next.length && prev.every((i, index) => i === next[index]) }
   )
   return { searchedItems }
