8304b886a2781aee084749dac53dee3b0cb30e14
edsoalter <edsolater@qq.com>
2023-08-18 10:49:00 +0800

    ✨ createStateVariable

--- src/packages/fnkit/createStateVariable.ts
+++ src/packages/fnkit/createStateVariable.ts
@@ -2,15 +2,17 @@ import { createEventCenter } from '@edsolater/fnkit'
 
 type StateVariable<T> = {
   /** get current state */
-  (): T
+  currentValue: T
 
   /** used by consumer */
   onChange(cb: (newValue: T) => void): { unsubscribe(): void }
 
-  clearAllSubscribed(): void
+  clear(): void
 
   /** used by provider */
-  setValue(newValue: T | ((prev: T) => T)): void
+  emitValue(newValue: T | ((prev: T) => T)): void
+
+  [Symbol.toPrimitive](hint: 'number' | 'string' | 'default'): T | null
 }
 
 /**
@@ -21,11 +23,29 @@ export function createStateVariable<T>(defaultValue: T): StateVariable<T>
 export function createStateVariable<T>(defaultValue?: T): StateVariable<any> {
   let innerValue: T | undefined = defaultValue
   const eventCenter = createEventCenter<{
-    onValueSet(value: T): void
+    valueSet(value: T): void
   }>()
   return {
     onChange(cb: (newValue: T) => void) {
-      return eventCenter.on('onValueSet', cb)
+      return eventCenter.on('valueSet', cb)
+    },
+    clear() {
+      eventCenter.clear('valueSet')
+    },
+    emitValue(newValue: T | ((prev: T) => T)) {
+      if (typeof newValue === 'function') {
+        innerValue = (newValue as (prev: T) => T)(innerValue as T)
+      } else {
+        innerValue = newValue
+      }
+      eventCenter.emit('valueSet', [innerValue])
+    },
+    get currentValue() {
+      return innerValue
+    },
+
+    [Symbol.toPrimitive](hint) {
+      return innerValue
     },
   }
 }
