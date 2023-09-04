ed810d1745d8f635f37dde3698aed1e63e3891e5
edsoalter <edsolater@qq.com>
2023-09-04 23:08:58 +0800

    💡 type widget

--- src/packages/pivkit/components/Text.tsx
+++ src/packages/pivkit/components/Text.tsx
@@ -4,9 +4,15 @@ export type TextProps = KitProps<{
   inline?: boolean
   /** if true, it is 'text' */
   editable?: boolean | 'text' | 'all'
+  /**
+   *  all widgets should have `props:v`, to handle it's duty's property \
+   *  you should directily use `props.children` if possiable, this prop is for batch processing
+   */
+  value?: string | number
 }>
 
 /**
+ * @componentType widget
  * if for layout , inner content should only be text
  */
 export function Text(rawProps: TextProps) {
