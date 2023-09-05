86a05b62f5f0ff8d3b156a5293393e3aa95465b1
edsoalter <edsolater@qq.com>
2023-09-06 00:16:42 +0800

    🚧 can fetch bilibili api now

--- src/routes/api/bilibili/popular.ts
+++ src/routes/api/bilibili/popular.ts
@@ -1,7 +1,6 @@
 import { APIEvent, json } from 'solid-start'
 
-export async function GET({ params, fetch }: APIEvent) {
-  const res = await fetch('https://api.bilibili.com/x/web-interface/popular', {}).then(i=>i.text())
-  console.log('res: ', res)
-  return json({ res: 'hello' })
+export async function GET({ params }: APIEvent) {
+  const r2 = await globalThis.fetch('https://api.bilibili.com/x/web-interface/popular').then((res) => res.json())
+  return json(r2)
 }
