74c5e8eeb183c5738ca82d92462b25e8711d9087
edsolater <edsolater@qq.com>
2024-01-28 20:22:16 +0800

    🚧 Aside menu bar

---
+++ src/components/NavBox/AsideMenuBar.tsx
@@ -0,0 +1,63 @@
+import { createEffect, createMemo } from 'solid-js'
+import { threeGridSlotBoxICSS } from '../../icssBlocks/threeGridSlotBoxICSS'
+import { Box, Icon, KitProps, Loop, Piv, icssRow, renderHTMLDOM, useKitProps } from '../../packages/pivkit'
+import { useGlobalConfigContext } from '../../app'
+import { Link } from '../Link'
+import { useLocation } from '@solidjs/router'
+
+/**
+ * set document title
+ * @param title set document title
+ * @returns void
+ */
+function setMetaTitle(title?: string) {
+  if (title) {
+    window.document.title = title
+  }
+}
+
+/**
+ * {@link appConfig.navigator}
+ */
+export function AsideMenuBar(rawProps: KitProps) {
+  const { shadowProps } = useKitProps(rawProps, { name: AsideMenuBar.name })
+  const { appConfig, setAppConfig } = useGlobalConfigContext()
+
+  const { pathname } = useLocation()
+
+  const currentNavButton = createMemo(() => appConfig.navigator.navButtons.find((i) => i.path === pathname))
+
+  createEffect(() => {
+    setMetaTitle(currentNavButton()?.isHome ? appConfig.appName : `${appConfig.appName} | ${currentNavButton()?.name}`)
+  })
+
+  return (
+    <Piv<'nav'>
+      icss={[
+        icssRow({ items: 'center' }),
+        { userSelect: 'none', padding: '16px 32px', transition: '150ms' },
+        threeGridSlotBoxICSS,
+      ]}
+      shadowProps={shadowProps}
+      render:self={(selfProps) => renderHTMLDOM('nav', selfProps)}
+    >
+      <Box icss={icssRow({ gap: '32px' })}>
+        <Link href='/' innerRoute>
+          <Icon src='/logo-with-text.svg' icss={{ height: '32px' }} />
+        </Link>
+        {/* <Text icss={{ fontSize: '36px', fontWeight: '800px' }}>{props.title}</Text> */}
+      </Box>
+
+      {/* tabs */}
+      <Box icss={{ display: 'flex', gap: '16px' }}>
+        <Loop of={appConfig.navigator.navButtons} icss={{ display: 'flex', gap: '8px' }}>
+          {({ name, path }) => (
+            <Link href={path} innerRoute>
+              {name}
+            </Link>
+          )}
+        </Loop>
+      </Box>
+    </Piv>
+  )
+}
--- src/components/NavBox/index.tsx
+++ src/components/NavBox/index.tsx
@@ -1,6 +1,7 @@
 import { JSX } from 'solid-js'
 import { TopMenuBar } from './TopMenuBar'
 import { Box } from '../../packages/pivkit'
+import { AsideMenuBar } from './AsideMenuBar'
 
 export function NavBox({ content }: { content: JSX.Element }) {
   return (
@@ -11,12 +12,15 @@ export function NavBox({ content }: { content: JSX.Element }) {
         maxWidth: '100dvw',
         display: 'grid',
         gridTemplate: `
-          "nav" auto
-          "content" 1fr / 100dvw`,
+          "nav   nav" auto
+          "aside content" 1fr / 100px 1fr`,
       }}
     >
       <Box icss={{ gridArea: 'nav' }}>
         <TopMenuBar />
+      </Box>
+      <Box icss={{ gridArea: 'aside' }}>
+        <AsideMenuBar />
       </Box>
       <Box icss={{ gridArea: 'content' }}>{content}</Box>
     </Box>
